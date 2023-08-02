71f400ea59f895dca4e23a74565e176e011bcf78
farmio <farmio@alphart.net>
2023-08-02 08:36:05 +0200

    simplify init

--- templates/ha_integration_snippets/panel.py
+++ templates/ha_integration_snippets/panel.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 
 from typing import Final
 
+import panel_package_name as my_panel
 from homeassistant.components import panel_custom
 from homeassistant.core import HomeAssistant
-from panel_package_name import entrypoint_js, is_dev_build, locate_dir
 
 from .const import DOMAIN
 
@@ -16,17 +16,17 @@ async def register_panel(hass: HomeAssistant) -> None:
     """Register the Panel."""
     if DOMAIN not in hass.data.get("frontend_panels", {}):
         hass.http.register_static_path(
-            url_path=URL_BASE,
-            path=locate_dir(),
-            cache_headers=not is_dev_build(),
+            URL_BASE,
+            path=my_panel.locate_dir(),
+            cache_headers=my_panel.is_prod_build,
         )
         await panel_custom.async_register_panel(
             hass=hass,
             frontend_url_path=DOMAIN,
-            webcomponent_name="panel_frontend_name",  # custom element name from `main.ts`
+            webcomponent_name=my_panel.webcomponent_name,
             sidebar_title=DOMAIN.title(),
             sidebar_icon="mdi:human-greeting",
-            module_url=f"{URL_BASE}/{entrypoint_js()}",
+            module_url=f"{URL_BASE}/{my_panel.entrypoint_js}",
             embed_iframe=True,
             require_admin=True,
         )
